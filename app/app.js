9a313dbe3626760f1257073e60fd025154ab1281
matmath <mathieu.k.legault@gmail.com>
2015-06-27 13:01:36 -0400

    Adding controller for Router page
    Demo controller for router config

--- app/app.js
+++ app/app.js
@@ -48,3 +48,25 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 	        
 	    });
 }); // closes $routerApp.config()
+
+// let's define the scotch controller that we call up in the about state
+routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
