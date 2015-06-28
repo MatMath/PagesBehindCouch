1c792807bd7129ca2003676e3b0599bb3c619250
matmath <mathieu.k.legault@gmail.com>
2015-06-27 20:59:41 -0400

    Initial Build with Yo Angular Gulp generator

---
+++ readme.txt
@@ -0,0 +1,5 @@
+1: Making the routing system work
+2: Customising the pages with sub pages and partials
+3: Connect to a couchDB
+4: Create a login system
+5: Store that information
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pagesBehindCouch', ['ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
+
+})();
