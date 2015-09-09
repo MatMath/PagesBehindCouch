9c617e0e7f737095773662687f339973af482a91
matmath <mathieu.k.legault@gmail.com>
2015-09-09 13:46:26 -0400

    Initial working scheme with custom function in factory.

--- src/app/translation.config.js
+++ src/app/translation.config.js
@@ -4,19 +4,39 @@
 
 	angular
 		.module('pagesBehindCouch')
-		.config(['$translateProvider', function ($translateProvider) {
+		.config(['$translateProvider',
+			function($translateProvider) {
 
-		  $translateProvider.useStaticFilesLoader({
-		   		prefix: 'app/languages/',
-		   		suffix: '.json'
-		   });
-		 
-		  $translateProvider.preferredLanguage('en');
-		  $translateProvider.useLocalStorage();
-		  $translateProvider.useSanitizeValueStrategy('escape');
+				$translateProvider.useLoader('customLoader');
 
-		}]);
-	
+				$translateProvider.preferredLanguage('en');
+				$translateProvider.useLocalStorage();
+				$translateProvider.useSanitizeValueStrategy('escape');
 
+			}
+		]);
 })();
 
+(function() {
+	'use strict';
+	angular
+		.module('pagesBehindCouch')
+		.factory('customLoader', function($http, $q) {
+			// return loaderFn
+			return function(options) {
+				// step1: Get the right translation from the user CouchDB. 
+				// Step2: Merge the User/client translation with the Global one.
+				var deferred = $q.defer();
+				// do something with $http, $q and key to load localization files
+
+				var data = {
+					'TEXT': 'Fooooo'
+				};
+
+				deferred.resolve(data);
+				
+				// resolve with translation data
+				return deferred.promise;
+			};
+		});
+})();
